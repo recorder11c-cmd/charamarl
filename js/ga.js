a20eb0b80584d98fd7834037856fb1bdb6fa4a4d
YOSHIHARU KATO <KCL@MacBook-Pro-7.local>
2026-08-27 23:02:39 +0900

    計測+CVR改善: GA4基盤(ga.js全30頁)・商品ページにイベント+安心ブロック・ピン日本語化+販売中バッジ・トップに購入入口

---
+++ js/ga.js
@@ -0,0 +1,15 @@
+// CHARAMARL 計測 (GA4)
+// 測定ID(G-XXXXXXXXXX)をセットすると有効化。空なら何もしない(検算用Redisカウンターは別系統で常時動作)
+window.CM_GA_ID = '';
+(function(){
+  var id = window.CM_GA_ID; if(!id) return;
+  var s=document.createElement('script'); s.async=true;
+  s.src='https://www.googletagmanager.com/gtag/js?id='+id;
+  document.head.appendChild(s);
+  window.dataLayer=window.dataLayer||[];
+  window.gtag=function(){dataLayer.push(arguments);};
+  gtag('js', new Date());
+  gtag('config', id);
+})();
+// 共通イベント送信ヘルパー
+window.cmEvent=function(name,params){try{if(window.gtag)window.gtag('event',name,params||{});}catch(e){}};
